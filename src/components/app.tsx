7cdc503774d937c087c28a7b33f4f503c17e3169
Jason Brown <jason@Jasons-MacBook.local>
2017-02-04 14:42:41 +0100

    Initial boilerplate example.

---
+++ src/components/app.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+
+export default class App extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    public render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
---
+++ src/reducers/index.tsx
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+
+// state argument is not Application state, but only the Container state
+// that it is bound too.
+function defaultReducer(state: any = null, action: any) {
+    return state;
+}
+
+// The rootReducer is the overall application state is create from a set of "sub" states.
+// Each "sub"-state is defined by an individal reducer, which is a funtion that returns 
+// a state.
+const reducers = combineReducers({
+    default: defaultReducer
+});
+
+export {reducers};
