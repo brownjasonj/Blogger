14623e992d6322c52c94ada4396c04cdf888ac09
Jason Brown <brownjasonj@Jason-Browns-MacBook-Air.local>
2017-02-11 20:15:06 +0100

    Added new route for posting a blog Added new component for capturing a new post

---
+++ src/components/posts-new.component.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class PostsNew extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>Create New Post</div>
+    }
+}
+
+export { PostsNew }
